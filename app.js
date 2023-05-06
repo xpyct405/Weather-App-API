e312b3a0ac4777c40b4f5bf2e58df42c4238e3a5
Rustam Kuzyaev <xpyct405@yandex.ru>
2023-05-06 11:28:23 +0300

    feat: input reset rule

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 // https://codepen.io/namansingh98/pen/GRYmxjX
 
-// Request API OpenWearherMao.org
+
+// Request API OpenWearherMap.org
 const api_key = "5d9840e44a8ae22baed51974ec1bb853";
 const api_url = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
-
 const searchBox = document.querySelector("#input");
 const searchBtn = document.querySelector("#search-btn");
+
 async function checkWeather(city) {
   const api = await fetch(api_url + city + `&appid=${api_key}`);
   var data = await api.json();
-
 
   // console.log(data);
   document.querySelector(".city").innerHTML = data.name;
@@ -17,40 +17,64 @@ async function checkWeather(city) {
   document.querySelector(".humidity").innerHTML = data.main.humidity + " %";
   document.querySelector(".wind").innerHTML = data.wind.speed + " км/ч";
   document.querySelector(".pressure").innerHTML = Math.round( eval(data.main.pressure * 0.750062) ) + " мм рт. ст.";
-
+  // Очистим содержимое input
+  searchBox.value = '';
 }
 
 
 
-// Разрешим ввод только букв рус, англ и пробела
-document.querySelector("#input").addEventListener('keydown', function(e) {
-  if (e.key.match(/[^a-zA-ZА-Яа-яЁё' ']/)) return e.preventDefault()
+// Разрешим ввод только букв рус, англ, пробела и тире
+searchBox.addEventListener('keydown', function(e) {
+  if (e.key.match(/[^a-zA-ZА-Яа-яЁё' '-]/)) return e.preventDefault()
 }); // Будет перехватывать все числа при ручном вводе. 
 
-document.querySelector("#input").addEventListener('input', function(e) {
-  this.value = this.value.replace(/[^a-zA-ZА-Яа-яЁё' ']/g, "")
+searchBox.addEventListener('input', function(e) {
+  this.value = this.value.replace(/[^a-zA-ZА-Яа-яЁё' '-]/g, "")
 }) //Если ввели данные через копипаст или автозаполнение
 
 
 
 
-searchBtn.addEventListener("click", () => {
-  if (searchBox.value == '' || searchBox.value == undefined || searchBox.value == NaN  ) {
-    console.log('searchBox.value = эт чё');
-  } else {
-    checkWeather(searchBox.value);
-    
+// Разрешим производить поиск при клике по Enter
+searchBox.addEventListener('keydown', (event) => {
+  if (event.key == 'Enter') {
+    if (document.querySelector(".city").textContent === "undefined") {
+      alert(`We can't find ${checkbox.value} city! Check or retry one more time.`)
+      console.log(document.querySelector(".city").textContent);
+    }
+    else {
+      checkWeather(searchBox.value);
+      console.log(document.querySelector(".city").textContent);
+    }
   }
-// Reset input.value
-// searchBox.value = '';
+})
+
+
+// Разрешим производить поиск при клике по Enter
+searchBtn.addEventListener("click", () => {
+  if (document.querySelector(".city").textContent === "undefined") {
+    alert(`We can't find ${checkbox.value} city! Check or retry one more time.`)
+    console.log(document.querySelector(".city").textContent);
+  } else {
+    checkWeather(searchBox.value);   
+    console.log(document.querySelector(".city").textContent);
+  }
 });
 
+// searchBox.value == '' || searchBox.value == undefined || searchBox.value == NaN || 
 
-// searchBox.addEventListener('keydown', (event) => {
-//   if (event.key == 'Enter') 
-//   checkWeather(searchBox.value);
-//   // searchBox.value = '';
-// })
+// Напишем функцию по чистке input=text через 3секунды
+function inputReset() {
+  searchBox.value = '';
+}
+
+
+
+
+
+
+
+
 
 
 checkWeather('Tambov');
